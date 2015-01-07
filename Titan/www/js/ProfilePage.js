53f9d8faaeb1d3511992e8a6e7710e89387799d5
dylanMcC <dylan.t.mccarthy@gmail.com>
2015-01-07 10:05:24 -0500

    Gg new pages

---
+++ Titan/www/js/ProfilePage.js
@@ -0,0 +1,31 @@
+document.getElementById("homeButton").addEventListener("click", homeButtonPress);
+
+//window.location.replace('...');
+function homeButtonPress()
+{
+  window.location="Home.html";
+}
+
+
+//swipe code for moving the menue to the left when swiped
+$(window).load(function(){
+        $("[data-toggle]").click(function() {
+          var toggle_el = $(this).data("toggle");
+          $(toggle_el).toggleClass("open-sidebar");
+        });
+         $("body").swipe({
+              swipeStatus:function(event, phase, direction, distance, duration, fingers)
+                  {
+                      if (phase=="move" && direction =="right") {
+    
+                          // $(".container").addClass("open-sidebar");
+                           //return false;
+                      }
+                      if (phase=="move" && direction =="left") {
+                           $(".container").removeClass("open-sidebar");
+                           return false;
+                      }
+                  }
+
+          }); 
+      });
