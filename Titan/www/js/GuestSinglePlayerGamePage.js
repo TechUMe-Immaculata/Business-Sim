e2b59a033f419f0d83c02099d0c4330ea0c9308a
QuentenL <qlundie@gmail.com>
2015-04-13 13:22:28 -0400

    test

--- Titan/www/js/GuestSinglePlayerGamePage.js
+++ Titan/www/js/GuestSinglePlayerGamePage.js
@@ -53,7 +53,7 @@ window.onload = function(){
 	{
 				// Boolean - Whether to animate the chart
 				animation: false,
-				responsive : true
+				responsive : false
 	};
 	
 	var ctx = document.getElementById("chart-area_1").getContext("2d");
@@ -445,3 +445,15 @@ return null;
 })
 
   };
+  
+  				$(function() {			
+					//Enable swiping...
+					$("#SinglePlayerInGame").swipe( {
+						//Generic swipe handler for all directions
+						swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
+							console.log("You swiped " + direction );	
+						},
+						//Default is 75px, set to 0 for demo so any distance triggers swipe
+					   threshold:0
+					});
+				});
