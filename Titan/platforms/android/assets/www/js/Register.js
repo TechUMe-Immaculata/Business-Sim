a81f0ae245d2949b3e96b4b07fb9c9987a96e527
quentenl <qlundie@gmail.com>
2015-04-15 20:21:39 -0400

    Revert "Master"

--- Titan/platforms/android/assets/www/js/Register.js
+++ Titan/platforms/android/assets/www/js/Register.js
@@ -1,24 +1,37 @@
-document.getElementById("guestPlayButton").addEventListener("click", guestPlayPress);
-document.getElementById("serverLoginButton").addEventListener("click", serverLoginPress);
-document.getElementById("registerButton").addEventListener("click", registerPress);
-document.getElementById("facebookLoginButton").addEventListener("click", facebookLoginPress);
+
+document.getElementById("enterButton").addEventListener("click", Register);
 //window.location.replace('...');
-function guestPlayPress()
-{
-	window.location="GuestGamePage.html";
-}
 
-function serverLoginPress()
-{
-  window.location="ServerLoginPage.html";
-}
 
-function registerPress()
-{
-  window.location="Register.html";
-}
+function Register()
+{ 
+Parse.initialize("Z8KSlQyzuWQKn449idqkqNYbiH7HWy09US0ws0Ci", "zDzVGtrgvtFN0Sxs6YjkuOq9leznJ4UguavX6bdt");
+    
 
-function facebookLoginPress()
-{
-  window.location="Register.html";
-}
+  var username = this.$("#userName").val();
+  var password = this.$("#myPass").val();
+  var email = this.$("#myemail").val();
+  
+  
+    
+    
+  var user = new Parse.User();
+  user.set("name" , username);
+  user.set("username", username);
+  user.set("password", password);
+  user.set("email", email);
+  
+  user.signUp(null, {
+  success: function(user) {
+    // Hooray! Let them use the app now.
+  //alert("asfdasdfsadf");
+  //make call to create match
+  window.location="company.html";
+  },
+  error: function(user, error) {
+    // Show the error message somewhere and let the user try again.
+    alert("Error: " + error.code + " " + error.message);
+  }
+});
+  
+}; 
