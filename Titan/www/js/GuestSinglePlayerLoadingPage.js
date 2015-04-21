368dfe635bb3a9a6c295e0d549d1c94dfcd07873
QuentenL <qlundie@gmail.com>
2015-04-21 00:02:50 -0400

    QoL

--- Titan/www/js/GuestSinglePlayerLoadingPage.js
+++ Titan/www/js/GuestSinglePlayerLoadingPage.js
@@ -40,3 +40,13 @@ Parse.Cloud.run('createMatch', running, {
 
 
 };
+function makeMatchId()
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 6; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text=text+"_guest";
+}
