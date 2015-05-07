76f4c6a12c7e6695c2d1e14edb59061bb17fe992
mardpaul <mard.paulo@gmail.com>
2015-05-07 13:00:58 -0400

    done some work
    done some work

--- Titan/www/js/GuestSinglePlayerKahootJoin.js
+++ Titan/www/js/GuestSinglePlayerKahootJoin.js
@@ -16,10 +16,10 @@ else if ( rowCount <4)
 */
 
  var currentUser = Parse.User.current();
-
+var password = $('#password').val();
 	var  running = {};
 running.objectId = currentUser.id;
-running.password = "76991355";
+running.password = password;
 
 
 
@@ -36,8 +36,95 @@ Parse.Cloud.run('createMatch_Multi_Join', running, {
 		console.log("Nah boi");
 	}
 });
+// get the companyId so you can use it later for the query
+var Company = Parse.Object.extend("Company");
+var userId = currentUser.id;
+var companyId;
+nquery = new Parse.Query("Company");
+
+nquery.equalTo("userId", userId);
+
+nquery.find({
+   
+   //find the Active Turns in the match 
+    success: function(Company) {
+    	
+
+		companyId = Company[0].get("objectId");
+		console.log("Yes I got the company id")
 
 
+  },
+  error: function(error) {
+    alert("Error: " + error.code + " " + error.message);
+    console.log("not working for company Turns")
+  }
+});
 
+
+// code for user to check when hes ready 
+setInterval(function MultiplayerTurns(){
+
+var CompMatch = Parse.Object.extend("CompMatch");
+
+
+query = new Parse.Query("CompMatch");
+
+query.equalTo("password", password);
+query.equalTo("companyId", companyId);
+query.find({
+   
+   //find the Active Turns in the match 
+    success: function(CompMatch) {
+    	console.log(CompMatch);
+
+		if (CompMatch[0].get("isSubbed") == false){
+
+			window.location = "GuestMultiplayerGamePage.html";
+		
+		}
+		else if (CompMatch[0].get("isSubbed") == true ){
+			
+		}	
+
+
+  },
+  error: function(error) {
+    alert("Error: " + error.code + " " + error.message);
+    console.log("not working for the CompMatch Turns");
+  }
+});
+///
+var CompMatch = Parse.Object.extend("CompMatch");
+
+
+query = new Parse.Query("CompMatch");
+
+query.equalTo("password", password);
+query.equalTo("companyId", companyId);
+query.find({
+   
+   //find the Active Turns in the match 
+    success: function(CompMatch) {
+    	console.log(CompMatch);
+
+		if (CompMatch[0].get("isSubbed") == false){
+
+			window.location = "GuestMultiplayerGamePage.html";
+		
+		}
+		else if (CompMatch[0].get("isSubbed") == true ){
+			
+		}	
+
+
+  },
+  error: function(error) {
+    alert("Error: " + error.code + " " + error.message);
+    console.log("not working for the CompMatch Turns");
+  }
+});
+
+} , 10000);
 
 };
